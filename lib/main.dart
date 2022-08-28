cd76f43023f52f8bef7cc0e55fbbf7d7c6630176
JJunio <junio.araujo01@yahoo.com>
2022-08-28 06:57:14 -0300

    Atualizando projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Carmen Sandiego',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Onde Está Carmen'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +32,107 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Marker> _pontos = [];
+  double _zomm = 6;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _exiberMapa(_pontos, _zomm);
+  }
+
+  Widget _exiberMapa(List<Marker> _pontos, _zomm) {
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(-8.4121861, -38.9033043),
+        zoom: _zomm,
+      ),
+      layers: [
+        TileLayerOptions(
+          urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+          userAgentPackageName: 'com.example.app',
+        ),
+        MarkerLayerOptions(
+          markers: _pontos,
+        ),
+      ],
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'JJunio',
+          onSourceTapped: null,
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Expanded(
+              child: Container(
+                child: Column(
+                  children: [
+                    Flexible(
+                      child: _exiberMapa(_pontos, _zomm),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _pontos.add(
+                        Marker(
+                          point: LatLng(-8.4121861, -38.9033043),
+                          width: 80,
+                          height: 80,
+                          builder: (context) =>
+                              const Icon(Icons.pin_drop),
+                        ),
+                      );
+                      _pontos.add(
+                        Marker(
+                          point: LatLng(-8.5421001,-37.0922099),
+                          width: 80,
+                          height: 80,
+                          builder: (context) =>
+                              const Icon(Icons.pin_drop),
+                        ),
+                      );
+                      _pontos.add(
+                        Marker(
+                          point: LatLng(-8.3675989,-37.5671426),
+                          width: 80,
+                          height: 80,
+                          builder: (context) =>
+                              const Icon(Icons.pin_drop),
+                        ),
+                      );
+                      _pontos.add(
+                        Marker(
+                          point: LatLng(-8.3141311,-38.0768195),
+                          width: 80,
+                          height: 80,
+                          builder: (context) =>
+                              const Icon(Icons.pin_drop),
+                        ),
+                      );
+                      _zomm = 10.0;
+                      _exiberMapa(_pontos, _zomm);
+                    });
+                  },
+                  child: const Text('Get Location?')),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
